b12dae8f98d3dcfbb8ea65f2a5c3277b231d8811
Harri Feng <harrifeng@gmail.com>
2015-12-26 21:26:42 +0800

    001 two sum

---
+++ 001_two_sum.rb
@@ -0,0 +1,28 @@
+require 'test/unit'
+
+class MyTest < Test::Unit::TestCase
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_two_sum
+    assert_equal [1, 2],  two_sum([2, 7, 11, 15], 9)
+  end
+end
+
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  m = {}
+  nums.each_with_index do |n, i|
+    if m[target - n] != nil
+      return [i + 1, m[target - n] + 1].minmax
+    else
+      m[n] = i
+    end
+  end
+end
