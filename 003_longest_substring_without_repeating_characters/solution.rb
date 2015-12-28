0773663b925633667a0b88c7d6d000090c1292cc
Haoran Feng <haoran.feng@sap.com>
2015-12-28 12:25:47 +0800

    time limit exceeded

---
+++ 003_longest_substring_without_repeating_characters/solution.rb
@@ -0,0 +1,38 @@
+require 'test/unit'
+
+class MyTest < Test::Unit::TestCase
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_two_sum
+    assert_equal 3, length_of_longest_substring("abcabcbb")
+    assert_equal 2, length_of_longest_substring("aab")
+    assert_equal 1, length_of_longest_substring("bbbbb")
+  end
+end
+
+
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)
+  index = Array.new(256, -1)
+  mx = 0
+  len = 0
+  i  = 0
+
+  while i < s.length do
+    if index[s[i].ord] != -1
+      mx = [mx, len].max
+      len = 0
+      i = index[s[i].ord] + 1
+      index.collect!{|n| n = -1}
+    end
+    len += 1
+    index[s[i].ord] = i
+    i += 1
+  end
+  return [mx, len].max
+end
