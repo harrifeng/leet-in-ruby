8c698918bf474a9bffae847ce6bc2ff25ccc9388
Harri Feng <harrifeng@gmail.com>
2016-01-01 11:19:05 +0800

    new accepted

---
+++ 009_palindrome_number/solution.rb
@@ -0,0 +1,38 @@
+require 'test/unit'
+
+class MyTest < Test::Unit::TestCase
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_two_sum
+    assert_true is_palindrome(2332)
+    assert_true is_palindrome(20002)
+    assert_false is_palindrome(-2332)
+  end
+end
+
+# @param {Integer} x
+# @return {Boolean}
+def is_palindrome(x)
+  if x < 0
+    return false
+  end
+  size = 1
+  tmp = x
+  while tmp > 9 do
+    size *= 10
+    tmp /= 10
+  end
+
+  while size > 9 do
+    if x / size != x % 10
+      return false
+    end
+    x = x % size / 10
+    size /= 100
+  end
+  return true
+end
