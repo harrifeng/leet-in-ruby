45fb56e832cb92459c9728487daab542a2ed63d3
Haoran Feng <haoran.feng@sap.com>
2016-01-04 20:55:38 +0800

    new accepted

---
+++ 014_longest_common_prefix/solution.rb
@@ -0,0 +1,33 @@
+# coding: utf-8
+require 'test/unit'
+
+class MyTest < Test::Unit::TestCase
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_two_sum
+    assert_equal "ab", longest_common_prefix(["abcd", "abccc", "abbbb"])
+    assert_equal "wab", longest_common_prefix(["wabcdsssc", "wabccc", "wabbbb"])
+  end
+end
+
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+  if strs.length == 0
+    return ""
+  end
+  ret = ""
+  for i in (0..strs[0].length-1) do
+    for j in (1..strs.length-1) do
+      if strs[j].length < i + 1 || strs[0][i] != strs[j][i]
+        return ret
+      end
+    end
+    ret += strs[0][i]
+  end
+  return ret
+end
