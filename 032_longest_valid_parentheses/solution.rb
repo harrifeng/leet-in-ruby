84ff859365dbea5a0e9436e47713b70ca524c891
Haoran Feng <haoran.feng@sap.com>
2016-01-20 17:16:45 +0800

    new accepted

--- 032_longest_valid_parentheses/solution.rb
+++ 032_longest_valid_parentheses/solution.rb
@@ -11,26 +11,30 @@ class MyTest < Test::Unit::TestCase
     assert_equal 4, longest_valid_parentheses(")()())")
     assert_equal 2, longest_valid_parentheses("(()")
     assert_equal 2, longest_valid_parentheses("()(()")
+    assert_equal 0, longest_valid_parentheses("")
+    assert_equal 8, longest_valid_parentheses("((()))())")
+    assert_equal 4, longest_valid_parentheses(")()())()()(")
   end
 end
 
 # @param {String} s
 # @return {Integer}
 def longest_valid_parentheses(s)
+  if s.length == 0
+    return 0
+  end
   count = [0] * s.length
   stack = []
   for i in (0..s.length-1) do
     if s[i] == '('
       stack.push s[i]
     elsif s[i] == ')'
-      if stack.size > 0
+      if stack.size == 0 || stack.last == ')'
+        stack.push s[i]
+      else
         stack.pop
-        sum = 2
-        for j in (stack.size..i) do
-          sum += count[j]
-          count[j] = 0
-        end
-        count[stack.size] += sum
+        count[stack.size] += count[stack.size+1] + 2
+        count[stack.size+1] = 0
       end
     end
   end
