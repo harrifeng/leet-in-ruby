07c135b1b3a55aefa59d3ed4b38acdb883f24c97
Haoran Feng <haoran.feng@sap.com>
2016-01-30 16:21:28 +0800

    not working

--- 037_sudoku_solver/solution.rb
+++ 037_sudoku_solver/solution.rb
@@ -8,12 +8,10 @@ class MyTest < Test::Unit::TestCase
   end
 
   def test_leet_function
-    # b1 = ["..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."]
-    # r1 = ["519748632","783652419","426139875","357986241","264317598","198524367","975863124","832491756","641275983"]
-    # solve_sudoku(b1)
-    # assert_equal r1, b1
-    assert empty_multi_array([[]])
-    assert !empty_multi_array([[],[8]])
+    b1 = ["..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."]
+    r1 = ["519748632","783652419","426139875","357986241","264317598","198524367","975863124","832491756","641275983"]
+    solve_sudoku(b1)
+    assert_equal r1, b1
   end
 end
 
@@ -38,9 +36,9 @@ def solve_sudoku(board)
   for i in (0..8)
     for j in (0..8)
       if board[i][j] != '.'
-        lines[i].delete board[i][j].to_i
-        columns[j].delete board[i][j].to_i
-        groups[i/3*3+j/3].delete board[i][j].to_i
+        lines[i].delete board[i][j]
+        columns[j].delete board[i][j]
+        groups[i/3*3+j/3].delete board[i][j]
       end
     end
   end
@@ -56,17 +54,8 @@ def do_sudoku(lines, columns, groups, board)
   for i in (0..8)
     for j in (0..8)
       if board[i][j] == '.'
-        lines[i].each do |l|
-          if columns[j].include?(l) && group[i/3*3 + j/3].include?(l)
-            lines[i].delete(l)
-            columns[j].delete(l)
-            group[i/3*3+j/3].delete(l)
-            do_sudoku(lines, columns, groups, board)
-            lines[i] << l
-            columns[j] << l
-            group[i/3*3+j/3] << l
+        for k in (lines[i].length-1).downto(0) do
           end
-        end
       end
     end
   end
@@ -79,5 +68,5 @@ def empty_multi_array(arr)
       return false
     end
   end
-  xreturn true
+  return true
 end
