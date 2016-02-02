a349caa5eae1535cc4f77d9e0004f0c117a8b759
Harri Feng <harrifeng@gmail.com>
2016-02-02 20:08:17 +0800

    byebug

--- 041_missing_positive/solution.rb
+++ 041_missing_positive/solution.rb
@@ -8,41 +8,32 @@ class MyTest < Test::Unit::TestCase
   end
 
   def test_leet_function
-    # assert_equal 2, first_missing_positive([1, 1])
+    assert_equal 2, first_missing_positive([1, 1])
     assert_equal 2, first_missing_positive([3, 4, -1, 1])
-    assert_equal 3, first_missing_positive([1, 2, 0])
-    assert_equal 9, first_missing_positive([1, 3, 4, 8 , 6, 5, 7, 2, 10, 12, 11])
+    # assert_equal 3, first_missing_positive([1, 2, 0])
+    # assert_equal 9, first_missing_positive([1, 3, 4, 8 , 6, 5, 7, 2, 10, 12, 11])
   end
 end
 
 # @param {Integer[]} nums
 # @return {Integer}
 def first_missing_positive(nums)
-  if nums.length == 0
-    return 0
-  end
-  minv = nums[0]
-  maxv = nums[0]
-  nums.each do |n|
-    if n < minv
-      minv = n
-    end
-    if n > maxv
-      maxv = n
+  n = nums.length
+  i = 0
+  while i < n do
+    if (nums[i] > 0) && (nums[i] < n) && (nums[i] != i) && (nums[nums[i]] != nums[i])
+      p nums
+      nums[i], nums[nums[i]] = nums[nums[i]], nums[i]
+    else
+      i += 1
     end
   end
 
-  sum = 0
-  nums.each do |n|
-    sum = sum ^ n
-  end
-  (minv..maxv).each do |e|
-    sum = sum ^ e
+  for i in (1..n-1)
+    if nums[i] != i
+      return i
+    end
   end
 
-  if sum == 0
-    return minv > 1 ? minv - 1 : maxv + 1
-  else
-    return sum
-  end
+  return nums[0] == n ? n + 1 : n;
 end
