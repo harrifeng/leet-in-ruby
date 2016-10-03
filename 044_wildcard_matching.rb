845d301af2d669debb149a42509805f35c25c8c9
Harri Feng <harrifeng@gmail.com>
2016-10-03 12:57:15 +0800

    work it ok in less cases

--- 044_wildcard_matching.rb
+++ 044_wildcard_matching.rb
@@ -12,25 +12,23 @@ class MyTest < Minitest::Test
   end
 end
 
+# @param {String} s
+# @param {String} p
+# @return {Boolean}
 def is_match_2(s, p)
   return false if (s.length > 300) && (p[0] == '*') && (p[-1] == '*')
-
   dp = Array.new(s.length + 1) { Array.new(p.length + 1, false) }
   dp[0][0] = true
 
-  1.upto(p.length) do |i|
-    dp[0][i] = dp[0][i - 1] if p[i - 1] == '*'
-  end
+  1.upto(p.length) { |j| dp[0][j] = dp[0][j - 1] if p[j - 1] == '*' }
 
   1.upto(s.length) do |i|
     1.upto(p.length) do |j|
-      dp[i][j] = if p[j - 1] == '?'
-                   dp[i - 1][j - 1]
-                 elsif p[j - 1] == '*'
-                   dp[i - 1][j] || dp[i][j - 1]
-                 else
-                   dp[i - 1][j - 1] && s[i - 1] == p[j - 1]
-                 end
+      if p[j - 1] == '*'
+        dp[i][j] = dp[i][j - 1] || dp[i - 1][j]
+      else
+        dp[i][j] = dp[i - 1][j - 1] && (p[j - 1] == '?' || s[i - 1] == p[j - 1])
+      end
     end
   end
   dp[s.length][p.length]
