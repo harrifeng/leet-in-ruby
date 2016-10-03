f53774cf623adffafa368a03f056ec703056079b
Harri Feng <harrifeng@gmail.com>
2016-10-03 13:37:54 +0800

    new understand

--- 047_permutations_ii.rb
+++ 047_permutations_ii.rb
@@ -4,25 +4,35 @@ require 'minitest/autorun'
 class MyTest < Minitest::Test
   def test_leet_047
     assert_equal [[1, 1, 2], [1, 2, 1], [2, 1, 1]].sort,
-                 permute_ii([1, 1, 2]).sort
+                 permute_unique([1, 1, 2]).sort
   end
 end
 
-def permute_ii(nums)
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute_unique(nums)
   ret = []
   helper_047(nums.sort, 0, ret)
   ret
 end
 
 def helper_047(nums, level, ret)
-  if level == nums.length
-    ret.push(nums.dup)
+  if nums.length == level
+    ret.push nums.dup
     return
   end
-  (level...nums.length).each do |i|
-    next unless (i == level) || !nums[level...i].include?(nums[i])
+  level.upto(nums.length - 1) do |i|
+    next if nums[level...i].include?(nums[i])
     nums[level], nums[i] = nums[i], nums[level]
     helper_047(nums, level + 1, ret)
     nums[level], nums[i] = nums[i], nums[level]
   end
 end
+
+
+###########################################################################
+#                  [1, 1, 2, 2]                                           #
+#                   ^                                                     #
+# [1, 1, 2, 2] [1, 1, 2, 2] [2, 1, 1, 2] [(1, 1, 2) (2)]                  #
+#     ^         duplicated      ^         drop it becasue 2 in (1, 1, 2)  #
+###########################################################################
