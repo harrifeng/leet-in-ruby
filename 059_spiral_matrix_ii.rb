815eb89a1cdc61d9e01c0dd8b4cf71a5f33aa10c
Harri Feng <harrifeng@gmail.com>
2016-10-04 18:33:31 +0800

    at least works

---
+++ 059_spiral_matrix_ii.rb
@@ -0,0 +1,49 @@
+require 'minitest/autorun'
+
+# MiniTest class
+class MyTest < Minitest::Test
+  def test_leet_059
+    assert_equal [[1, 2, 3], [8, 9, 4], [7, 6, 5]], generate_matrix(3)
+  end
+end
+
+# @param {Integer} n
+# @return {Integer[][]}
+def generate_matrix(n)
+  helper(n, 0)
+end
+
+def helper(n, count)
+  return [[]] if n.zero?
+  return [[count + 1]] if n == 1
+
+  matrix = Array.new(n) { Array.new(n, 0) }
+
+  matrix[0][0...-1] = matrix[0][0...-1].map do
+    count += 1
+  end
+
+  matrix[0...-1].map do |line|
+    count += 1
+    line[-1] = count
+    line
+  end
+
+  tmp = matrix[-1][1..-1].reverse.map { count += 1 }
+  matrix[-1][1..-1] = tmp.reverse
+
+  matrix[1..-1].reverse.map do |line|
+    count += 1
+    line[0] = count
+    line
+  end
+
+  inner = helper(n - 2, count)
+  return matrix if inner == [[]]
+  n.times do |i|
+    n.times do |j|
+      matrix[i][j] = inner[i - 1][j - 1] if matrix[i][j].zero?
+    end
+  end
+  matrix
+end
