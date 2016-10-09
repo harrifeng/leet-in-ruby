d1242e2933a80b45de0cd62899bebc2eb21c39d4
Harri Feng <harrifeng@gmail.com>
2016-10-09 21:21:25 +0800

    4th

--- 097_interleaving_string.rb
+++ 097_interleaving_string.rb
@@ -16,23 +16,22 @@ end
 def is_interleave(s1, s2, s3)
   m = s1.length
   n = s2.length
-  k = s3.length
-
-  return false if m + n != k
-
+  return false if (m + n) != s3.length
   dp = Array.new(m + 1) { Array.new(n + 1, false) }
+  dp[0][0] = true
 
-  (m + 1).times do |i|
-    (n + 1).times do |j|
-      dp[i][j] = if i.zero? && j.zero?
-                   true
-                 elsif i > 0 && dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]
-                   true
-                 elsif j > 0 && dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]
-                   true
-                 else
-                   false
-                 end
+  1.upto(m) do |i|
+    dp[i][0] = dp[i - 1][0] && (s1[i - 1] == s3[i - 1])
+  end
+
+  1.upto(n) do |j|
+    dp[0][j] = dp[0][j - 1] && (s2[j - 1] == s3[j - 1])
+  end
+
+  1.upto(m) do |i|
+    1.upto(n) do |j|
+      dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) ||
+                 (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1])
     end
   end
   dp[m][n]
