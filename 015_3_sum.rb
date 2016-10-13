e51c86c4b8d70fd78f92af3bd03320cf6fdb8c77
Haoran Feng <haoran.feng@alo7.com>
2016-10-13 17:30:47 +0800

    solve 1st

--- 015_3_sum.rb
+++ 015_3_sum.rb
@@ -11,16 +11,17 @@ end
 def three_sum(nums)
   nums.sort!
   ret = []
-  (nums.length - 2).times do |i|
-    next if (i > 0) && (nums[i] == nums[i - 1])
+
+  0.upto(nums.length - 3) do |i|
+    next if i > 0 && nums[i] == nums[i - 1]
     front = i + 1
     back = nums.length - 1
+
     while front < back
-      cnt = nums[i] + nums[front] + nums[back]
+      cnt = nums[front] + nums[back] + nums[i]
       if cnt.zero?
         ret.push([nums[i], nums[front], nums[back]])
-        while front < back && nums[front] == nums[back] &&
-              nums[back] == nums[back - 1]
+        while front < back && (nums[front] == nums[front + 1]) && (nums[back] == nums[back - 1])
           front += 1
           back -= 1
         end
