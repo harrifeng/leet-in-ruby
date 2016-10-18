a00e77d96bd84a3efcd4732017e46b25a1394917
Haoran Feng <haoran.feng@alo7.com>
2016-10-18 09:25:44 +0800

    solve 1st

--- 007_reverse_integer.rb
+++ 007_reverse_integer.rb
@@ -14,13 +14,13 @@ end
 def reverse(x)
   sign = 1
   sign = -1 if x < 0
-
   x = x.abs
   ret = 0
+
   while x > 0
     ret = ret * 10 + x % 10
     x /= 10
   end
-  return 0 if ret > 2**31 - 1
+  return 0 if ret > (2**31 - 1)
   sign * ret
 end
