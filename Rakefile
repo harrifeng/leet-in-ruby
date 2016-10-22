ea0a9dee066142f94fe060856fa825c26a666614
Harri Feng <harrifeng@gmail.com>
2016-10-22 11:19:57 +0800

    works, but too slow, need new solution

--- Rakefile
+++ Rakefile
@@ -1,15 +1,38 @@
 require 'rake/testtask'
+require 'git'
 
 Rake::TestTask.new(:test) do |t|
   t.test_files = FileList['./*.rb']
 end
 
+def get_file_last_commit(file_name)
+  file_commit = `git log -1 --format=oneline -- #{file_name}`
+  file_commit[0...40]
+end
+
+def get_commit_branch_list(commit_sha)
+  arr_str = `git branch --contains #{commit_sha}`
+  arr_str.split(' ').map{|a| a if a != '*' }.compact
+end
+
+def get_current_branch
+  g = Git.open('.')
+  g.branches.map{|b| b if b.current }.compact[0].to_s
+end
+
 desc 'random question'
 task :rand do
-  rb_files = Dir['[0-9]*.rb']
-  rb_files.map! do |r|
-    [r.slice(0, 3).to_i, r]
-  end
+  rb_raw_files = Dir['[0-9]*.rb']
+  rb_files = rb_raw_files.map { |r|
+    file_in_branches = get_commit_branch_list(get_file_last_commit(r))
+    if file_in_branches.include?(get_current_branch) && !file_in_branches.include?('master')
+      [r.slice(0, 3).to_i, r]
+    else
+      nil
+    end
+  }.compact
+
+  puts "Still have #{rb_files.length} / #{rb_raw_files} to finish"
 
   rand_file = rb_files[(rand * rb_files.size).to_i][1]
 
--- test.rb
+++ test.rb
@@ -1,23 +1,55 @@
 require 'git'
 require 'logger'
 
-g = Git.open('.', :log => Logger.new(STDOUT))
-g = Git.open('.')
+def get_file_last_commit(file_name)
+  file_commit = `git log -1 --format=oneline -- #{file_name}`
+  file_commit[0...40]
+end
 
-g.index
-g.index.readable?
-# g.log.each { |l| puts l }
-ret = g.branches.map{|b| b if b.current }.compact[0]
-master = g.branches["master"]
+def get_commit_branch_list(commit_sha)
+  arr_str = `git branch --contains #{commit_sha}`
+  arr_str.split(' ').map{|a| a if a != '*' }.compact
+end
 
-starter = `git merge-base #{ret.gcommit.sha} #{master.gcommit.sha}`
-starter.chop!
+def get_current_branch
+  g = Git.open('.')
+  g.branches.map{|b| b if b.current }.compact[0]
+end
 
-puts `git diff #{starter} #{ret.gcommit.sha}  --stat`
+puts get_current_branch
+puts get_commit_branch_list(get_file_last_commit('Gemfile'))
 
-# bb =  `git merge-base --is-ancestor #{ret.gcommit.sha} #{master.gcommit.sha}`
-# puts bb.length
+# g = Git.open('.', :log => Logger.new(STDOUT))
+# g = Git.open('.')
+#
+# g.index
+# g.index.readable?
+# # g.log.each { |l| puts l }
+# ret = g.branches.map{|b| b if b.current }.compact[0]
+# master = g.branches["master"]
+#
+# starter = `git merge-base #{ret.gcommit.sha} #{master.gcommit.sha}`
+# starter.chop!
+#
+# puts starter
+#
+# file_name = "001_two_sum.rb"
+#
+# file_commit = `git log -1 --format=oneline -- #{file_name}`
+# file_commit = file_commit[0...40]
+# puts file_commit
 #
 #
-# cc =  `git merge-base --is-ancestor #{aa} #{ret.gcommit.sha}`
-# puts cc.length
+# ans1= `git merge-base --is-ancestor #{starter} #{file_commit} `
+# ans2= `git merge-base --is-ancestor #{file_commit} #{starter} `
+# ans2= `git merge-base --is-ancestor HEAD~ HEAD `
+# puts "ans is #{ans1}, #{ans2}"
+#
+# puts `git merge-base --is-ancestor #{ret.gcommit.sha} #{master.gcommit.sha}`
+# puts `git merge-base --is-ancestor #{master.gcommit.sha} #{ret.gcommit.sha} `
+#
+# arr_str = `git branch --contains 7411a7c53b13ea9050bc74c030036e71585837d2`
+#
+# arr = arr_str.split(' ').map{|a| a if a != '*' }.compact
+#
+# puts arr
