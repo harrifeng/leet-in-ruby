23ba2f9f4855cddbf3a41cc82dce2a9111f0a906
Haoran Feng <haoran.feng@alo7.com>
2016-10-25 09:41:20 +0800

    solve 1st, to master, a better way to pass by value of hash instead of Fixnum

--- 052_n_queens_ii.rb
+++ 052_n_queens_ii.rb
@@ -3,37 +3,41 @@ require 'minitest/autorun'
 # MiniTest class
 class MyTest < Minitest::Test
   def test_leet_052
-    assert_equal 2, solve_n_queens(4)
-    assert_equal 10, solve_n_queens(5)
+    assert_equal 2, total_n_queens(4)
+    assert_equal 10, total_n_queens(5)
   end
 end
 
-def solve_n_queens(n)
-  @ret = 0
-  helper_052(n, 0, [])
-  @ret
+# @param {Integer} n
+# @return {Integer}
+def total_n_queens(n)
+  ret = { val: 0 }
+  helper_052(n, 0, [], ret)
+  ret[:val]
 end
 
-def queen_valid?(queen, row, col)
-  row.times do |i|
-    queen[0].length.times do |j|
-      return false if queen[i][j] == 'Q' &&
-                      (j == col || (row - i).abs == (col - j).abs)
+def valid?(board, x, y)
+  return true if board.length.zero?
+  x.times do |i|
+    board[0].length.times do |j|
+      return false if (board[i][j] == 'Q') &&
+                      (j == y || (x - i).abs == (y - j).abs)
     end
   end
   true
 end
 
-def helper_052(n, level, tmp)
+def helper_052(n, level, tmp, ret)
   if level == n
-    @ret += 1
+    ret[:val] += 1
     return
   end
+
   line = '.' * n
   n.times do |i|
     cur_line = line[0...i] + 'Q' + line[i + 1...n]
-    tmp.push(cur_line)
-    helper_052(n, level + 1, tmp) if queen_valid?(tmp, level, i)
+    tmp.push cur_line
+    helper_052(n, level + 1, tmp, ret) if valid?(tmp, level, i)
     tmp.pop
   end
 end
