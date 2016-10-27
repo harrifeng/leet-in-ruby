32dba9b2e93067ac8c56a308c30ab3c197b36c78
Haoran Feng <haoran.feng@alo7.com>
2016-10-27 17:24:51 +0800

    solve 1st

--- 093_restore_ip_addresses.rb
+++ 093_restore_ip_addresses.rb
@@ -12,23 +12,26 @@ end
 # @param {String} s
 # @return {String[]}
 def restore_ip_addresses(s)
-  return [] if s.length.zero?
   ret = []
-  helper_093(s, [], ret)
+  helper_093(s, 0, [], ret)
   ret
 end
 
-def helper_093(s, tmp, ret)
-  return if tmp.length > 4
-  if s.length.zero? && tmp.length == 4
-    ret.push(tmp.join('.'))
+def helper_093(s, level, tmp, ret)
+  if level == 4 && s.length.zero?
+    ret.push tmp.join('.')
     return
   end
 
-  1.upto([s.length, 3].min) do |i|
-    next if (i > 1 && (s[0] == '0')) || (i == 3 && s[0...3].to_i > 255)
-    tmp.push s[0...i]
-    helper_093(s[i...s.length], tmp, ret)
+  if level > 4
+    return
+  end
+
+  [s.length,3].min.times do |i|
+    cur_s = s.slice(0, i + 1)
+    next if (cur_s[0] == '0' && cur_s.length > 1) || cur_s.to_i > 256
+    tmp.push(cur_s)
+    helper_093(s[cur_s.length...s.length], level + 1, tmp, ret)
     tmp.pop
   end
 end
