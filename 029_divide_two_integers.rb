48d4df93a01908c431f57786905b257b440d84b5
Haoran Feng <haoran.feng@alo7.com>
2016-12-05 15:33:38 +0800

    move to master, better and correct soluton found a previous bug

--- 029_divide_two_integers.rb
+++ 029_divide_two_integers.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'minitest/autorun'
 
 # Test for solution
@@ -5,6 +6,7 @@ class MyTest < Minitest::Test
   def test_leet_029
     assert_equal 5, divide(15, 3)
     assert_equal 5000, divide(15_000, 3)
+    assert_equal 2_147_483_647, divide(-2_147_483_648, -1)
   end
 end
 
@@ -12,36 +14,30 @@ end
 # @param {Integer} divisor
 # @return {Integer}
 def divide(dividend, divisor)
-  is_neg = (dividend > 0) != (divisor > 0)
-
+  sign = if dividend.positive? != divisor.positive?
+           -1
+         else
+           1
+         end
   a = dividend.abs
   b = divisor.abs
 
   ret = 0
-  while (a >= b) do
-    # ret += 1
-    # a -= b
+
+  while a >= b
     c = b
-    pos = 0
-    while (a >= c) do
+    i = 0
+    while a >= c
       a -= c
-      ret =  ret + (1 << pos)
-      pos += 1
+      ret += (1 << i)
+      i += 1
       c = (c << 1)
     end
   end
 
-  if is_neg
-    if ret > 2 ** 31 - 1
-      return -2 ** 31
-    else
-      ret * -1
-    end
-  else
-    if ret > 2 ** 31 - 1
-      return 2 ** 31 - 1
-    else
-      return ret
-    end
-  end
+  imin = -2**31
+  imax = 2**31 - 1
+  return imax if ret * sign > imax
+  return imin if ret * sign < imin
+  ret * sign
 end
